001ba3e08b1692d0fb891141f58c4e45c6a3ab27
kknowles3 <53347001+kknowles3@users.noreply.github.com>
2025-06-07 10:58:37 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import dash
+from dash import html
+import libs.private_lib.hello as private
+
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    html.H1("Main App"),
+    html.Div(private.say_hello())
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
